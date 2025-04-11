a5b31ffcac237cf2a9f2c4aabe329fba5e7b25e7
Aluno IBMR <alunoibmr@animaedu.intranet>
2025-04-11 11:10:02 -0300

    feat: spring boot

---
+++ backend/rmjsoftware/src/main/java/com/rmjsoftware/RmjsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.rmjsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmjsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmjsoftwareApplication.class, args);
+	}
+
+}
---
+++ backend/rmjsoftware/src/test/java/com/rmjsoftware/RmjsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmjsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmjsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
